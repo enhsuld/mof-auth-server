264101ea39236983ed9787aece4d244c446a32b4
enhsuld <z.enhsuld@yahoo.com>
2017-09-28 18:55:40 +0800

    from work

--- src/main/java/com/macro/dev/config/CustomTokenEnhancer.java
+++ src/main/java/com/macro/dev/config/CustomTokenEnhancer.java
@@ -5,16 +5,26 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.macro.dev.models.TcUser;
+import com.macro.dev.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("organization", authentication.getName() + randomAlphabetic(4));
+        TcUser usr=userService.getUser(authentication.getName());
+        additionalInfo.put("username", authentication.getName());
+        additionalInfo.put("organizationId", usr.getTcOrg().getOrgCd());
+        additionalInfo.put("organization", usr.getTcOrg().getOrgNm());
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
